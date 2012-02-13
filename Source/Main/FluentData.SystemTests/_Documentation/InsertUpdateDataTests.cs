dadcca2cbf2ab10663a4f1c30e1fad97e84aeb3d
SND\kindblad_cp <SND\kindblad_cp@aadf2aa7-86a3-4242-a263-039aa7a476c3>
2012-02-13 21:53:04 +0000

    git-svn-id: https://fluentdata.svn.codeplex.com/svn@12018 aadf2aa7-86a3-4242-a263-039aa7a476c3

--- Source/Main/FluentData.SystemTests/_Documentation/InsertUpdateDataTests.cs
+++ Source/Main/FluentData.SystemTests/_Documentation/InsertUpdateDataTests.cs
@@ -6,27 +6,31 @@ namespace FluentData._Documentation
 	[TestClass]
 	public class InsertUpdateDataTests : BaseDocumentation
 	{
+		[TestMethod]
 		public void Test()
 		{
-			//var product = new Product();
-			//product.Name = "The Warren Buffet Way";
-			//product.CategoryId = 1;
+			var product = new Product();
+			product.Name = "The Warren Buffet Way";
+			product.CategoryId = 1;
 
-			//var insertBuilder = Context().Insert<Product>("Product", product);
-			//FillBuilder(insertBuilder);
-			//var productId = insertBuilder.Execute();
+			var insertBuilder = Context().Insert<Product>("Product", product);
+			FillBuilder((IInsertUpdateBuilder<Product>) insertBuilder);
+			product.ProductId = insertBuilder.ExecuteReturnLastId();
 
-			//Assert.IsTrue(productId > 0);
+			Assert.IsTrue(product.ProductId > 0);
 
-			//var updateBuilder = Context().Update<Product>("Product", product);
-			//FillBuilder(updateBuilder);
+			var updateBuilder = Context().Update<Product>("Product", product);
+			FillBuilder((IInsertUpdateBuilder<Product>) updateBuilder);
 
-			//int rowsAffected = updateBuilder.Execute();
+			int rowsAffected = updateBuilder.Where(x => x.ProductId).Execute();
+
+			Assert.IsTrue(rowsAffected > 0);
 		}
 
 		public void FillBuilder(IInsertUpdateBuilder<Product> builder)
 		{
-			
+			builder.Column(x => x.Name);
+			builder.Column(x => x.CategoryId);
 		}
 	}
 }
