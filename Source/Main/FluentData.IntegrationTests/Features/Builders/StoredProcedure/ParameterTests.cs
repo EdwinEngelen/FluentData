44279f85f7a376544f1dbc4dc3ea4465c22ac002
SND\kindblad_cp <SND\kindblad_cp@aadf2aa7-86a3-4242-a263-039aa7a476c3>
2012-05-13 21:07:11 +0000

    Fixed issue with stored procedure builder and output parameters.
    git-svn-id: https://fluentdata.svn.codeplex.com/svn@17033 aadf2aa7-86a3-4242-a263-039aa7a476c3

---
+++ Source/Main/FluentData.IntegrationTests/Features/Builders/StoredProcedure/ParameterTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentData._Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentData.Features.Builders.StoredProcedure
+{
+	[TestClass]
+	public class ParameterTests
+	{
+		[TestMethod]
+		public void Test_Output()
+		{
+			var builder = TestHelper.Context().StoredProcedure("TestOutputParameter").ParameterOut("ProductName", DataTypes.String, 50);
+
+			builder.Execute();
+
+			var value = builder.ParameterValue<string>("ProductName");
+
+			Assert.IsNotNull(value);
+		}
+	}
+}
